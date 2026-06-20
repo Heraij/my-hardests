1802f6038b22cbaa661ffed016414374a2e4d5d0
Heraij <adam.hajek@zsmschotoviny.cz>
2026-06-20 14:33:28 +0200

    Add files via upload

---
+++ demonlist-data.js
@@ -0,0 +1,62 @@
+const DEMONLIST_DATA = [
+    {
+        "level": "Sakupen Egg",
+        "creator": "Sivlol",
+        "difficulty": "Medium Demon",
+        "video": "https://www.youtube.com/watch?v=M61Vqi6ryj8"
+    },
+    {
+        "level": "Reanimation",
+        "creator": "Terron",
+        "difficulty": "Medium Demon",
+        "video": "https://www.youtube.com/watch?v=RB8g4CZLik4&list=RDRB8g4CZLik4&start_radio=1"
+    },
+    {
+        "level": "Decode",
+        "creator": "Rek3dge",
+        "difficulty": "Easy Demon",
+        "video": "https://www.youtube.com/watch?v=IRTQZZ502J0&list=RDIRTQZZ502J0&start_radio=1"
+    },
+    {
+        "level": "Deadlocked",
+        "creator": "RobTop",
+        "difficulty": "Easy Demon", // Changed to match your new gradient rules!
+        "video": "https://www.youtube.com/watch?v=I3LFTGTIWoI"
+    },
+    {
+        "level": "X",
+        "creator": "Triaxis",
+        "difficulty": "Easy Demon",
+        "video": "https://www.youtube.com/watch?v=t_2qpQGNS3A"
+    },
+      {
+        "level": "Platinum Adventure",
+        "creator": "Jerry4",
+        "difficulty": "Easy Demon",
+        "video": "https://www.youtube.com/watch?v=DWGGjNnU5CA"
+    },  
+    {
+        "level": "Clubstep",
+        "creator": "Robtop",
+        "difficulty": "Easy Demon",
+        "video": "https://www.youtube.com/watch?v=VInQkeec188"
+    },
+    {
+        "level": "Speed Racer",
+        "creator": "ZenthicAlpha",
+        "difficulty": "Easy Demon",
+        "video": "https://www.youtube.com/watch?v=R8RsJVKKd8w"
+    },
+     {
+        "level": "THE LIGHTNING ROAD",
+        "creator": "timeless real",
+        "difficulty": "Easy Demon",
+        "video": "https://www.youtube.com/watch?v=smwunc7UiHk"
+    },
+    {
+        "level": "The Nightmare",
+        "creator": "Jax",
+        "difficulty": "Easy Demon",
+        "video": "https://www.youtube.com/watch?v=i0dlZgqA8ds"
+    }
+];
